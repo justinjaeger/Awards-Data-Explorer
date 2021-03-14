eb283fbfa7b974291d3690085c52a2381fcead46
Justin Jaeger <jjustinjaeger@gmail.com>
2021-03-14 13:43:22 -0400

    switching branch

--- db_setup/db.sql
+++ db_setup/db.sql
@@ -22,6 +22,18 @@ CREATE TABLE `followers` (
   PRIMARY KEY (`username`, `follower`)
 );
 
+CREATE TABLE `nominees` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `awardsShow` VARCHAR(100) NOT NULL,
+  `year` VARCHAR(5) NOT NULL,
+  `category` VARCHAR(100) NOT NULL,
+  `subcategory` VARCHAR(100),
+  `film` VARCHAR(200) NOT NULL,
+  `nominee` VARCHAR(200) NOT NULL,
+  `winner` BIT(1) DEFAULT 0 NOT NULL
+);
+
+
 CREATE TABLE `awardsShows` (
   `awardsShow_id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) UNIQUE NOT NULL,
