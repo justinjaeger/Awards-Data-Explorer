46e1f29e710f61849fae16be87d4ffbd62a77d7d
Justin Jaeger <jjustinjaeger@gmail.com>
2021-05-17 18:58:42 -0400

    add settings for config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
+    root: true,
     env: {
         browser: true,
         es2021: true,
     },
-    extends: ["plugin:react/recommended", "airbnb"],
+    extends: ["plugin:react/recommended", "airbnb", "prettier"],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
@@ -11,7 +12,21 @@ module.exports = {
         ecmaVersion: 12,
         sourceType: "module",
     },
-    plugins: ["react"],
-    rules: {},
+    plugins: ["react", "import"],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                "quoteProps": "consistent",
+                "singleQuote": true,
+                "tabWidth": 4,
+                "trailingComma": "es5",
+                "useTabs": false
+            }
+        ]
+    },
     indent: ["error", "tab"],
+    eslintIgnore: [
+        "node_modules/",
+    ],
 };
