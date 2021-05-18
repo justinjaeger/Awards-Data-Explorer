779395f1bf6dbea326e0e9971ed3d0204a0b6c62
Justin Jaeger <jjustinjaeger@gmail.com>
2021-05-18 00:49:50 -0400

    wip

---
+++ pages/api/loginV2/index.js
@@ -0,0 +1,3 @@
+// GET: login existing user
+// POST: create new user
+// DELETE: logs user out
---
+++ pages/api/users/[id]/image.js
@@ -0,0 +1,3 @@
+// GET: profile image
+// POST: upload profile image
+// DELETE: delete profile image
