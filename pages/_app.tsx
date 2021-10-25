ae8a5323a218348f516f7deff206001f92d4f711
Justin Jaeger <jjustinjaeger@gmail.com>
2021-10-24 23:32:33 -0400

    fix eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,35 +13,33 @@ import theme from '../theme';
 /**
  * Component is the your page eg /index
  * pageProps is whatever you get from getServerSideProps in your component
- * 
- * Try to get this initial load down: 
+ *
+ * Try to get this initial load down:
  * https://dev.to/estruyf/devhack-optimizing-initial-load-js-from-next-js-5c3d
  * - experiment with dynamic imports
+ * - especially for user/[username]
  */
 
-const App = ({ Component, pageProps }: AppProps) => {
-    return (<>
-        <ThemeProvider theme={muiTheme}>
-            <SessionProvider session={pageProps.session}>
-                <AuthProvider>
-                    <NotificationProvider>
-                        <div
-                            style={{
-                                width: '100%',
-                                height: '100%',
-                                position: 'absolute',
-                                backgroundColor: theme.colors.white,
-                            }}
-                        >
-                            <Header />
-                            <Component {...pageProps} />
-                        </div>
-                    </NotificationProvider>
-                </AuthProvider>
-            </SessionProvider>
-        </ThemeProvider>
-        </>
-    );
-};
+const App = ({ Component, pageProps }: AppProps) => (
+    <ThemeProvider theme={muiTheme}>
+        <SessionProvider session={pageProps.session}>
+            <AuthProvider>
+                <NotificationProvider>
+                    <div
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            position: 'absolute',
+                            backgroundColor: theme.colors.white,
+                        }}
+                    >
+                        <Header />
+                        <Component {...pageProps} />
+                    </div>
+                </NotificationProvider>
+            </AuthProvider>
+        </SessionProvider>
+    </ThemeProvider>
+);
 
 export default App;
